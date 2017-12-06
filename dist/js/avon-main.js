adc46d32188c98cfaf20a0a6d9f82d3b09d65cb4
Nathan Hiemstra <nathanhiemstra@gmail.com>
2017-12-05 20:56:51 -0600

    Finishing basics on Product Detail

--- dist/js/avon-main.js
+++ dist/js/avon-main.js
@@ -1,1 +1,85 @@
-console.log('Hello from avon-main.js');
+
+$(document).ready(function() {
+
+////////////////////////////////////////////////////////
+// VARIABLES
+////////////////////////////////////////////////////////
+
+
+  var selectors = {
+    main:                       "#avon-poc",
+    triggers: {
+      drawerOrderSummary:       ".trigger-drawer-order-summary"
+    },
+    drawers: {
+      orderSummary:             ".drawer-order-summary"
+    }
+  }
+  var $objects = {
+    window: $(window),
+    document: $(document),
+    body: $('body'),
+    main: $(selectors.main),
+    triggers: {
+      drawerOrderSummary:   $(selectors.main).find(selectors.triggers.drawerOrderSummary)
+    },
+    drawers: {
+      orderSummary:         $(selectors.main).find(selectors.drawers.orderSummary)
+    }
+  }
+
+
+
+  ////////////////////////////////////////////////////////
+  // FUNCTIONS
+  ////////////////////////////////////////////////////////
+
+  // function deepFind(obj, path) {
+  //   var paths = path.split('.'),
+  //     current = obj,
+  //     i;
+
+  //   for (i = 0; i < paths.length; ++i) {
+  //     if (current[paths[i]] == undefined) {
+  //       return undefined;
+  //     } else {
+  //       current = current[paths[i]];
+  //     }
+  //   }
+  //   return current;
+  // }
+
+ 
+ 
+
+
+  ////////////////////////////////////////////////////////
+  // LISTENERS
+  ////////////////////////////////////////////////////////
+
+  // $objects.inputs.rangeAll.on("input", function() {
+  //   recalculate();
+  // });
+
+  // $objects.inputs.aepOrCapacityFactor.on("change", function() {
+  //   recalculate();
+  // });
+
+
+  $objects.triggers.drawerOrderSummary.on("click", function() {
+    $objects.drawers.orderSummary.toggleClass('drawer-expanded');
+  });
+
+  // $objects.nav.continue.on("click", function() {
+  //   showStepMain();
+  // });
+
+
+  ////////////////////////////////////////////////////////
+  // INT
+  ////////////////////////////////////////////////////////
+
+ 
+
+
+});
