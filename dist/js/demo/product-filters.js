7ef2adcc74cdfd2c7e5cc771cc2863c2b8ce34e0
Nathan Hiemstra <nathanhiemstra@gmail.com>
2018-02-14 18:54:18 -0600

    Progress on Product Filters mobile - Adding Javascrpit

--- dist/js/demo/product-filters.js
+++ dist/js/demo/product-filters.js
@@ -3,8 +3,6 @@
  * @return {init} [description]
  */
 
-console.log('Hello'); 
-
 var ProductFilters = (function () {
 
   var $els = {};
@@ -14,26 +12,68 @@ var ProductFilters = (function () {
 
     // grab the DOM els we need
     $els = {
-      filterCheckboxes: $('.aside-product-filters .checkbox'),
+      filterCheckboxes: $('.aside-product-filters .checkbox-group :checkbox'),
+      checkboxGroup:    $('.aside-product-filters .checkbox-group'),
+      collapseBody:     $('.aside-product-filters .filter-collapse--body')
     };
-
-    console.log('init'); 
 
     _addListeners();
   };
 
   // private methods
   var _addListeners = function () {
-
-    $els.filterCheckboxes.on('click', function() {
+    $els.checkboxGroup.on('click', function() {
       _updateBadgeCount(this);
+    });
+
+    // Before show begins
+    $els.collapseBody.on('show.bs.collapse', function (e) {
+      _preventCollapsedForDesktop(e);
+    });
+
+    // Before collapse begins
+    $els.collapseBody.on('hide.bs.collapse', function (e) {
+      _preventCollapsedForDesktop(e);
+    });
+
+    // After collapse finished
+    $els.collapseBody.on('hidden.bs.collapse', function () {
+      _cleanupCollapsedForDesktop(this);
     });
 
   };
 
   var _updateBadgeCount = function(itemClicked) {
-    console.log('itemClicked: ',itemClicked); 
+    
+    // Find out how many checkboxes are checked
+    var $parent = $(itemClicked).parents('.form-group');
+    var $badge = $('.badge',$parent );
+    var checkedCount = $( "input:checked" ,$parent).length;
+    
+    // Update number in badge
+    $badge.text( checkedCount );
+    if ( checkedCount == 0 ) {
+      $badge.addClass('hidden-xs');
+    } else {
+      $badge.removeClass('hidden-xs');
+    }
   };
+
+  var _preventCollapsedForDesktop = function(e) {
+
+    // See if we're at "mobile" width by testing if this icon is visible
+    var numberOfVisibeIcons = $('.aside-product-filters .filter-collapse--icon:eq(0):visible').length;
+    
+    // If we're wider than mobile width, prevent collapse
+    if (numberOfVisibeIcons == 0) {
+      e.preventDefault();
+    }
+  }
+  
+  var _cleanupCollapsedForDesktop = function(itemCollpsed) {
+    // Remove inline style height:0 so if browser window is widened past mobile, checkboxes in a collapsed become visible
+    $(itemCollpsed).css( "height", "" );
+  }
 
 
   return {
@@ -41,6 +81,4 @@ var ProductFilters = (function () {
   };
 
 })();
-
-
 
